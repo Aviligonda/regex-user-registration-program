1976392442ae771c744875d98af2ac7098dbe66a
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-29 10:41:45 +0530

    Added : Check First name validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,11 @@
 package com.bridzlabz;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println(" Welcome to UserRegistration program using Regex ");
+        boolean firstName = Pattern.matches("[A-Z][a-z]{3,}", "Aviligonda");
+        System.out.println("FirstName : "+firstName);
     }
 }
